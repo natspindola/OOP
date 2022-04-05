7fbd62c7e72f52fdb7247a784131f37542212b60
natspindola <natspindola@hotmail.com>
2022-04-05 19:11:56 -0300

    segunda instância e atribuição de valores

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -9,13 +9,28 @@ namespace OOP
             //Criando instância da conta
             Conta conta1 = new Conta();
 
+            //Criando 2ª instância da conta
+            Conta conta2 = new Conta();
+
+            //Atribuindo valores para conta 1
             conta1.Saldo = 1500;
             conta1.Limite = 500;
             conta1.Numero = 123;
 
+            //Atribuindo valores para conta 2
+            conta2.Saldo = 2000;
+            conta2.Limite = 600;
+            conta2.Numero = 456;
+
             Console.WriteLine("Olá, seu saldo em conta é: " + conta1.Saldo);
             Console.WriteLine("O limite de saque é: " + conta1.Limite);
             Console.WriteLine("O número da conta é: " + conta1.Numero);
+
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.WriteLine("Olá, seu saldo em conta é: " + conta2.Saldo);
+            Console.WriteLine("O limite de saque é: " + conta2.Limite);
+            Console.WriteLine("O número da conta é: " + conta2.Numero);
         }
     }
 }
