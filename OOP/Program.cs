3d63006bb5f6e783f1e75c8c2270768f81a6029c
natspindola <natspindola@hotmail.com>
2022-04-06 17:56:11 -0300

    criando metodos pt2

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,30 +7,45 @@ namespace OOP
         static void Main(string[] args)
         {
             //Criando instância da conta
-            Conta conta1 = new Conta();
+            Conta conta = new Conta();
+
+            conta.Deposita(1500);
+
+            conta.Limite = 500;
+
+            conta.ConsultaSaldoDisponivel();
+            double saldo = conta.ConsultaSaldoDisponivel();
+
+            Console.WriteLine("Seu saldo é: " + saldo);
+
+
+            //Primeira forma
+
+            //Criando instância da conta
+            //Conta conta1 = new Conta();
 
             //Criando 2ª instância da conta
-            Conta conta2 = new Conta();
+            //Conta conta2 = new Conta();
 
             //Atribuindo valores para conta 1
-            conta1.Saldo = 1500;
-            conta1.Limite = 500;
-            conta1.Numero = 123;
+            //conta1.Saldo = 1500;
+            //conta1.Limite = 500;
+            //conta1.Numero = 123;
 
             //Atribuindo valores para conta 2
-            conta2.Saldo = 2000;
-            conta2.Limite = 600;
-            conta2.Numero = 456;
+            //conta2.Saldo = 2000;
+            //conta2.Limite = 600;
+            //conta2.Numero = 456;
 
-            Console.WriteLine("Olá, seu saldo em conta é: " + conta1.Saldo);
-            Console.WriteLine("O limite de saque é: " + conta1.Limite);
-            Console.WriteLine("O número da conta é: " + conta1.Numero);
+            //Console.WriteLine("Olá, seu saldo em conta é: " + conta1.Saldo);
+            //Console.WriteLine("O limite de saque é: " + conta1.Limite);
+            //Console.WriteLine("O número da conta é: " + conta1.Numero);
 
-            Console.WriteLine("-----------------------------------------------------------------");
+            //Console.WriteLine("-----------------------------------------------------------------");
 
-            Console.WriteLine("Olá, seu saldo em conta é: " + conta2.Saldo);
-            Console.WriteLine("O limite de saque é: " + conta2.Limite);
-            Console.WriteLine("O número da conta é: " + conta2.Numero);
+            //Console.WriteLine("Olá, seu saldo em conta é: " + conta2.Saldo);
+            //Console.WriteLine("O limite de saque é: " + conta2.Limite);
+            //Console.WriteLine("O número da conta é: " + conta2.Numero);
         }
     }
 }
