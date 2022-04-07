f09a1a43eb54d78f26984aa709bb6d5f44bd0173
natspindola <natspindola@hotmail.com>
2022-04-07 16:59:50 -0300

    herança pt1

---
+++ OOP/ContaPoupanca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class ContaPoupanca : Conta
+    {
+        public ContaPoupanca(int numero, double limite) : base(numero, limite)
+        {
+
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,63 +6,13 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Conta conta1 = new Conta(123, 500);
-            Conta conta2 = new Conta(456, 800);
-            Conta conta3 = new Conta(789, 900);
-            Conta conta4 = new Conta(987, 1000);
-
-            Console.WriteLine("Total de contas: " + Conta.TotalContasCriadas);
-
-            int total = Conta.ProximoTotalContasCriadas();
-            Console.WriteLine("Próximo total de contas: " + total);
-
             
-
-            ////Criando um objeto de cada classe
-
-            //CartaoDeCredito cdc = new CartaoDeCredito();
-            //Cliente c = new Cliente();
-
-            ////Adicionando o nome do cliente
-            //c.Nome = "Natália Spindola";
-
-            ////Adicionando o número e data de validade do cartão
-            //cdc.Numero = "1234 5678 9012 3456";
-            //cdc.DataDeValidade = "04/2026";
-            //cdc.Cliente = c;
-
-            //Console.WriteLine("O nome do cliente é: " + c.Nome);
-            //Console.WriteLine("O número do cartão é: " + cdc.Numero);
-            //Console.WriteLine("A validade do cartão é: " + cdc.DataDeValidade);
-            //Console.WriteLine("O nome do cliente do agregado é: " + cdc.Cliente.Nome);
-
-            
-
-            ////Criando instância da conta com métodos
-            //Conta conta = new Conta(123, 300);
-
-            //conta.AdicionarLimite(1500);
-
-            //conta.Deposita(1500);
-            //conta.Deposita(2500);
-            //conta.Deposita(4500);
-
-            //bool saca = conta.Saca(7000); //saque maior que saldo dá valor indisponível
-
-            //if (saca)
-            //{
-            //    double saldo = conta.ConsultaSaldoDisponivel();
-
-            //    Console.WriteLine("Seu saldo é: " + saldo);
-            //    Console.WriteLine("Seu limite é: " + conta.Limite);
-            //    Console.WriteLine("O número da sua conta é: " + conta.Numero);
-            //}
         }
     }
 }
 
 
-//Primeira forma - sem usar método
+////Primeira forma - sem usar método
 
 //Criando instância da conta
 //Conta conta1 = new Conta();
@@ -89,3 +39,66 @@ namespace OOP
 //Console.WriteLine("Olá, seu saldo em conta é: " + conta2.Saldo);
 //Console.WriteLine("O limite de saque é: " + conta2.Limite);
 //Console.WriteLine("O número da conta é: " + conta2.Numero);
+
+
+
+
+
+////Criando instância da conta com métodos
+//Conta conta = new Conta(123, 300);
+
+//conta.AdicionarLimite(1500);
+
+//conta.Deposita(1500);
+//conta.Deposita(2500);
+//conta.Deposita(4500);
+
+//bool saca = conta.Saca(7000); //saque maior que saldo dá valor indisponível
+
+//if (saca)
+//{
+//    double saldo = conta.ConsultaSaldoDisponivel();
+
+//    Console.WriteLine("Seu saldo é: " + saldo);
+//    Console.WriteLine("Seu limite é: " + conta.Limite);
+//    Console.WriteLine("O número da sua conta é: " + conta.Numero);
+//}
+
+
+
+
+
+//// Criando um objeto de cada classe
+
+//CartaoDeCredito cdc = new CartaoDeCredito();
+//Cliente c = new Cliente();
+
+////Adicionando o nome do cliente
+//c.Nome = "Natália Spindola";
+
+////Adicionando o número e data de validade do cartão
+//cdc.Numero = "1234 5678 9012 3456";
+//cdc.DataDeValidade = "04/2026";
+//cdc.Cliente = c;
+
+//Console.WriteLine("O nome do cliente é: " + c.Nome);
+//Console.WriteLine("O número do cartão é: " + cdc.Numero);
+//Console.WriteLine("A validade do cartão é: " + cdc.DataDeValidade);
+//Console.WriteLine("O nome do cliente do agregado é: " + cdc.Cliente.Nome);
+
+
+
+
+
+//// Métodos e atributos estáticos
+
+//Conta conta1 = new Conta(123, 500);
+//Conta conta2 = new Conta(456, 800);
+//Conta conta3 = new Conta(789, 900);
+//Conta conta4 = new Conta(987, 1000);
+
+//Console.WriteLine("Total de contas: " + Conta.TotalContasCriadas);
+
+//int total = Conta.ProximoTotalContasCriadas();
+//Console.WriteLine("Próximo total de contas: " + total);
+
